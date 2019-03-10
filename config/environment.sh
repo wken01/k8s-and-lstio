50b55554b196e7c38547b30794fefcaf72d3c0b3
wken01 <yuanxuemin-398@163.com>
2019-03-10 22:58:10 +0800

    Update environment.sh

--- config/environment.sh
+++ config/environment.sh
@@ -1,6 +1,58 @@
 #!/usr/bin/bash
 
-```Java
-public static void main()
-```
-[baidu]:http://www.baidu.com/img/bdlogo.gif "百度Logo"
+# 生成 EncryptionConfig 所需的加密 key
+export ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+
+# 集群各机器 IP 数组
+export NODE_IPS=(192.168.3.3 192.168.3.4 192.168.3.6)
+
+# 集群各 IP 对应的 主机名数组
+export NODE_NAMES=(kys-master.com kys-node1.com kys-node2.com )
+
+# etcd 集群服务地址列表
+export ETCD_ENDPOINTS="https://192.168.3.3:2379,https://192.168.3.4:2379,https://192.168.3.6:2379"
+
+# kube-apiserver 的反向代理(kube-nginx)地址端口
+export KUBE_APISERVER="https://127.0.0.1:8443"
+
+# 节点间互联网络接口名称
+export IFACE="ens33"
+
+# etcd 数据目录
+export ETCD_DATA_DIR="/data1/etcd"
+
+# k8s 各组件数据目录
+export K8S_DIR="/data1/k8s"
+
+# docker 数据目录
+export DOCKER_DIR="/data1/k8s/docker"
+
+## 以下参数一般不需要修改
+
+# TLS Bootstrapping 使用的 Token，可以使用命令 head -c 16 /dev/urandom | od -An -t x | tr -d ' ' 生成
+BOOTSTRAP_TOKEN="41f7e4ba8b7be874fcff18bf5cf41a7c"
+
+# 最好使用 当前未用的网段 来定义服务网段和 Pod 网段
+# 服务网段，部署前路由不可达，部署后集群内路由可达(kube-proxy 保证)
+SERVICE_CIDR="10.254.0.0/16"
+
+# Pod 网段，建议 /16 段地址，部署前路由不可达，部署后集群内路由可达(flanneld 保证)
+CLUSTER_CIDR="172.30.0.0/16"
+
+# 服务端口范围 (NodePort Range)
+export NODE_PORT_RANGE="30000-32767"
+
+# flanneld 网络配置前缀
+export FLANNEL_ETCD_PREFIX="/kubernetes/network"
+
+# kubernetes 服务 IP (一般是 SERVICE_CIDR 中第一个IP)
+export CLUSTER_KUBERNETES_SVC_IP="10.254.0.1"
+
+# 集群 DNS 服务 IP (从 SERVICE_CIDR 中预分配)
+export CLUSTER_DNS_SVC_IP="10.254.0.2"
+
+# 集群 DNS 域名（末尾不带点号）
+export CLUSTER_DNS_DOMAIN="cluster.local"
+
+# 将二进制目录 /opt/k8s/bin 加到 PATH 中
+export PATH=/k8s/kubernetes/bin:/k8s/etcd/bin:$PATH
