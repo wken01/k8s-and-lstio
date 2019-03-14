3c2fc49904631d63cc7b2b6068e1d0803b4c4cba
wken01 <yuanxuemin-398@163.com>
2019-03-14 22:07:39 +0800

    Update clearetcd.sh

--- config/clearetcd.sh
+++ config/clearetcd.sh
@@ -3,5 +3,5 @@ systemctl stop etcd
 systemctl daemon-reload
 rm -rf /var/lib/etcd
 rm -rf /usr/lib/systemd/system/etcd.service
-rm -rf /k8s/etcd
-rm -rf /data1
+rm -rf /k8s/etcd/
+rm -rf /data1/etcd/*
